a418e26559d8b001795f2c9f11baeed4c47d52c1
1000yearsago <118640132+1000yearsago@users.noreply.github.com>
2023-03-15 20:16:56 +0200

    Add files via upload

---
+++ task.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const path1 = 'stream/text.txt'
+
+function readStream(path){  // Функція, яка читає текстовий файл
+    const reader = fs.createReadStream(path)
+    reader.on('data',function (chunk){
+        console.log(chunk.toString())
+    })
+}
+
+function countwordsStream(path){  // Функція, яка рахує кількість слів в файлі
+    const amount = 0;
+    const reader = fs.createReadStream(path)
+    reader.on('data', function (chunk){
+        console.log(chunk.toString().split(" ").length)
+    })
+}
+
+readStream(path1)
+countwordsStream(path1)
